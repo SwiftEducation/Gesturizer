50b6843bdafbd115213fc553506d362e7a24ce62
ericgiannini <eric.giannini@yahoo.com>
2016-12-04 18:45:48 -0800

    Update to Xcode 8 and Swift 3.0

--- Gesturizer/ViewController.swift
+++ Gesturizer/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        singleTapRecognizer.requireGestureRecognizerToFail(doubleTapRecognizer)
+        singleTapRecognizer.require(toFail: doubleTapRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,47 +23,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    override func canBecomeFirstResponder() -> Bool {
+    override var canBecomeFirstResponder : Bool {
         return true
     }
     
-    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
-        if motion == .MotionShake {
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
             showGestureName("Shake")
         }
     }
     
-    @IBAction func singleTap(sender: UITapGestureRecognizer) {
+    @IBAction func singleTap(_ sender: UITapGestureRecognizer) {
         showGestureName("Tap")
     }
     
-    @IBAction func doubleTap(sender: UITapGestureRecognizer) {
+    @IBAction func doubleTap(_ sender: UITapGestureRecognizer) {
         showGestureName("Double Tap")
     }
     
-    func showGestureName(name: String) {
+    func showGestureName(_ name: String) {
         gestureName.text = name
-        UIView.animateWithDuration(1.0,
+        UIView.animate(withDuration: 1.0,
             animations: { self.gestureName.alpha = 1.0 },
             completion: { _ in
-                UIView.animateWithDuration(1.0) { self.gestureName.alpha = 0 }
+                UIView.animate(withDuration: 1.0, animations: { self.gestureName.alpha = 0 }) 
         })
     }
     
-    @IBAction func pinch(sender: UIPinchGestureRecognizer) {
-        if sender.state == .Ended {
+    @IBAction func pinch(_ sender: UIPinchGestureRecognizer) {
+        if sender.state == .ended {
             showGestureName("Pinch")
         }
     }
     
-    @IBAction func rotation(sender: UIRotationGestureRecognizer) {
-        if sender.state == .Ended {
+    @IBAction func rotation(_ sender: UIRotationGestureRecognizer) {
+        if sender.state == .ended {
             showGestureName("Rotation")
         }
     }
     
-    @IBAction func swipe(sender: UISwipeGestureRecognizer) {
-        if sender.direction == .Right {
+    @IBAction func swipe(_ sender: UISwipeGestureRecognizer) {
+        if sender.direction == .right {
             showGestureName("Right Swipe")
         }
     }
